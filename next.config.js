035bf396a1fa705bcfd74b821754af664711e5a6
Adson Amorim <adson.nascimento@wisereducacao.com>
2023-02-05 23:26:31 -0300

    feat: initial config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,29 @@
 /** @type {import('next').NextConfig} */
+const { NextFederationPlugin } = require("@module-federation/nextjs-mf");
+
 const nextConfig = {
   experimental: {
     appDir: true,
   },
-}
+  webpack: (config, options) => {
+    const { isServer } = options;
+    config.experiments = { topLevelAwait: true, layers: true };
 
-module.exports = nextConfig
+    config.plugins.push(
+      new NextFederationPlugin({
+        name: "shared",
+        filename: "static/chuncks/sharedEntry.js",
+        extraOptions: {
+          automaticAsyncBoundary: true,
+          exposePages: true,
+        },
+        exposes: {
+          "./button": "./src/components/Button",
+        },
+      })
+    );
+    return config;
+  },
+};
+
+module.exports = nextConfig;
---
+++ src/components/Button/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Button as MUIButton } from "@mui/material";
+
+type ButtonProps = {};
+
+export default function Button() {
+  return <MUIButton />;
+}
